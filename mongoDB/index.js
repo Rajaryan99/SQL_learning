358df565efbf1dcb6c4e6ca70eeae98ee4863fcc
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-23 15:50:46 +0530

    created read , update, delete routs form database

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -9,13 +9,34 @@ app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-app.get('/create', (req, res) => {
-    usermodel.create({
-        name: "Raj",
-        email: 'rajaryan@gmail.com',
-        username:"@raj_aryan"
-    })
+app.get('/create', async (req, res) => {
+    let newuser = await usermodel.create({
+        name: "Raju",
+        email: 'raju@gmail.com',
+        username: "@raju"
+    },
+    )
+
+    res.send(newuser)
 })
+
+app.get('/update', async (req, res) => {
+    let updateuser = await usermodel.findOneAndUpdate({ username: "@raj_aryan" }, { name: "Raj Aryan" }, { new: true });
+    res.send(updateuser)
+})
+
+
+app.get('/read', async (req, res) => {
+    let user = await usermodel.find();
+    res.send(user)
+})
+
+
+app.get('/delete', async (req, res) => {
+    let user = await usermodel.findOneAndDelete({ username: "@raju" });
+    res.send(user)
+})
+
 
 
 app.listen(port, () => {
