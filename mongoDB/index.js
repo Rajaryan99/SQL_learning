5113f83919f05d3d49fa4927bc9216404ca60102
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-24 11:43:27 +0530

    mongobd with ejs

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,42 +1,46 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const userModel = require('./models/user');
 
-const usermodel = require('./usermodel')
 
 const port = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set("views", path.join('views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-    res.send('hello world');
-})
-
-app.get('/create', async (req, res) => {
-    let newuser = await usermodel.create({
-        name: "Raju",
-        email: 'raju@gmail.com',
-        username: "@raju"
-    },
-    )
-
-    res.send(newuser)
-})
-
-app.get('/update', async (req, res) => {
-    let updateuser = await usermodel.findOneAndUpdate({ username: "@raj_aryan" }, { name: "Raj Aryan" }, { new: true });
-    res.send(updateuser)
-})
-
+    res.render('index')
+});
 
 app.get('/read', async (req, res) => {
-    let user = await usermodel.find();
-    res.send(user)
+    let users = await userModel.find();
+    res.render('read', { users });
+});
+
+app.post('/create', async (req, res) => {
+    try {
+        let { name, email, image } = req.body;
+
+        let createdUser = await userModel.create({
+            name,
+            email,
+            image
+        });
+
+        console.log("user Created:", createdUser);
+        res.redirect('/read')
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ error: err.message });
+    }
+
 })
-
-
-app.get('/delete', async (req, res) => {
-    let user = await usermodel.findOneAndDelete({ username: "@raju" });
-    res.send(user)
-})
-
 
 
 app.listen(port, () => {
